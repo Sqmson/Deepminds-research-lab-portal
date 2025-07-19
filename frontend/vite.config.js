2e9a5baa70bc6b88ec3e1b12e54e1d6c245f178f
Sqmson <mandresamson103@gmail.com>
2025-07-19 19:01:19 +0300

    vite env revert to default

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,10 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return {
-    plugins: [react(), tailwindcss()],
-    server: {
-      proxy: {
-        '/articles': {
-          target: env.VITE_API_BASE_URL,
-          changeOrigin: true,
-          secure: false,
-        },
-      },
-    },
-  };
-});
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),
+    tailwindcss(),
+  ],
+})
